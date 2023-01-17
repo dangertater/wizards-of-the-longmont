a052fbcbaed70803b3dd86a7d8e74e9931280736
dangertater <106702944+dangertater@users.noreply.github.com>
2023-01-17 11:27:52 -0700

    antD button working, will use antD as ui library for ze practice

--- src/Components/CustomButton.tsx
+++ src/Components/CustomButton.tsx
@@ -1,7 +1,16 @@
 import React from "react"
+import { Button, Space } from "antd"
 
-let StyledCustomButton = styled("button")``
+let CustomAntDButton: React.FC = () => (
+	<Space wrap>
+		<Button type="primary">
+			TODO: onclick this will generate timeline shit from the other branch
+		</Button>
+	</Space>
+)
 
 export function CustomButton() {
-	return <StyledCustomButton></StyledCustomButton>
+	return <CustomAntDButton></CustomAntDButton>
 }
+
+// https://ant.design/components/button
